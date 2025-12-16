a900cce75cb38e40e4d13e59687c72af4ae37130
5init Smith <138648307+5init@users.noreply.github.com>
2025-12-16 22:42:39 +0700

    Remove GNOME Session

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+dnf5 -y remove gnome-session
 dnf5 install -y @cosmic-desktop-environment
 
 # Use a COPR Example:
@@ -22,6 +22,4 @@ dnf5 install -y @cosmic-desktop-environment
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
-systemctl disable gdm.service
-systemctl enable cosmic-greeter.service
+# systemctl enable podman.socket
